d714b6acc5b31208db752a28a878acd865417f11
michal.banka <michal.banka@nokia.com>
2019-07-11 15:00:30 +0200

    Set Oparent as parent and fix Checkstyle errors in sdc/dcae-d/dt
    Change-Id: I99c2ddd024976bff2f9544d6b4f183427fea3273
    Issue-ID: SDC-2440
    Signed-off-by: michal.banka <michal.banka@nokia.com>

--- src/main/java/org/onap/sdc/dcae/controller/proxy/DcaeProxyOld.java
+++ src/main/java/org/onap/sdc/dcae/controller/proxy/DcaeProxyOld.java
@@ -1,3 +1,22 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SDC
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
 package org.onap.sdc.dcae.controller.proxy;
 
 import com.google.common.cache.Cache;
@@ -19,15 +38,16 @@ import java.util.concurrent.TimeUnit;
 
 public class DcaeProxyOld extends ProxyServlet {
 
+    private static final int EXPIRATION_TIME = 10;
+    private static final int TIMEOUT = 180000;
     private static Logger log = LoggerFactory.getLogger(DcaeProxyOld.class);
-    private static Cache<String, MdcData> mdcDataCache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS).build();
-
+    private static Cache<String, MdcData> mdcDataCache = CacheBuilder.newBuilder().expireAfterWrite(EXPIRATION_TIME, TimeUnit.SECONDS).build();
 
     //TODO proper configuration class
     private String newFeUrl;
     private String newFeContextPath;
 
-    public DcaeProxyOld(String newFeUrl, String newFeContextPath){
+    public DcaeProxyOld(String newFeUrl, String newFeContextPath) {
         this.newFeUrl = newFeUrl;
         this.newFeContextPath = newFeContextPath;
     }
@@ -41,16 +61,16 @@ public class DcaeProxyOld extends ProxyServlet {
     @Override
     protected HttpClient createHttpClient() throws ServletException {
         HttpClient client = super.createHttpClient();
-        client.setIdleTimeout(180000);
-        client.setConnectTimeout(180000);
+        client.setIdleTimeout(TIMEOUT);
+        client.setConnectTimeout(TIMEOUT);
         return client;
     }
 
     @Override
     protected String rewriteTarget(HttpServletRequest request) {
-        try{
+        try {
             logRequest(request);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Unexpected FE request logging error :", e);
         }
         String uri = request.getRequestURI();
@@ -59,8 +79,9 @@ public class DcaeProxyOld extends ProxyServlet {
         String query = request.getQueryString();
         StringBuilder url = new StringBuilder();
         url.append(newFeUrl).append(uri);
-        if(null != query)
+        if (null != query) {
             url.append("?").append(query);
+        }
         String urlString = url.toString();
         log.info("Proxy outgoing request={}", urlString);
         return urlString;
@@ -68,9 +89,9 @@ public class DcaeProxyOld extends ProxyServlet {
 
     @Override
     protected void onProxyResponseSuccess(HttpServletRequest clientRequest, HttpServletResponse proxyResponse, Response serverResponse) {
-        try{
+        try {
             logResponse(clientRequest, serverResponse);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Unexpected FE response logging error :", e);
         }
         super.onProxyResponseSuccess(clientRequest, proxyResponse, serverResponse);
